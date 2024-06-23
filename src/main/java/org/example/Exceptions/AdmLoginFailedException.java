90f177575a2dde06b6d37a469af2f4361e8b7532
Brito <gustavo.ponciano2005@outlook.com>
2024-06-23 05:16:17 -0300

    exception de falha de login do adm

---
+++ src/main/java/org/example/Exceptions/AdmLoginFailedException.java
@@ -0,0 +1,9 @@
+package org.example.Exceptions;
+
+public class AdmLoginFailedException extends  Exception{
+
+        public AdmLoginFailedException(String message) {
+            super(message);
+        }
+
+}
