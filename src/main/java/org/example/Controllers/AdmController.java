a9866d54b8359956f001f0da94ed9e0171f532b7
Brito <gustavo.ponciano2005@outlook.com>
2024-06-23 05:16:35 -0300

    criação da funcção de login

--- src/main/java/org/example/Controllers/AdmController.java
+++ src/main/java/org/example/Controllers/AdmController.java
@@ -1,11 +1,47 @@
 package org.example.Controllers;
 
+import org.example.Daos.AdmDaos;
+import org.example.Exceptions.AdmLoginFailedException;
 import org.example.Models.Filme;
 import org.example.Models.Sessao;
+import org.example.Models.Usuario.Administrador;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Scanner;
 
 public class AdmController {
+
+    private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm");
+    private static final String ADM_FILE_NAME = "colocar caminho do arquivo";
+
+    public static List<Administrador> listarAdm(){
+        return AdmDaos.listarAdm(ADM_FILE_NAME);
+    }
+
+    public static Administrador login(String nome, String senha) throws AdmLoginFailedException {
+
+        try{
+            var listaAdm = listarAdm();
+
+            for(Administrador adm: listaAdm){
+                if(nome.equals(adm.getNome()) && senha.equals(adm.getSenha())){
+                    return adm;
+                }
+            }
+
+        }catch(Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        throw new AdmLoginFailedException("Nome ou senha incorretos");
+
+    }
+
+    public void registrar() {
+
+    }
 
     public static void adicionarFilme(Scanner sc){
         System.out.println("1- Digite o nome do filme");
@@ -13,13 +49,13 @@ public class AdmController {
         System.out.println("2- Digite o genero do filme");
         String genero = sc.nextLine();
         System.out.println("3- Digite o horario da sessao: (dd/mm/yy HH:mm)");
-        String data = sc.nextLine();
+        LocalDateTime horarioSessao = LocalDateTime.parse(sc.nextLine(), dtf);
         System.out.println("4- Digite o valor da sessao");
         Double valor = sc.nextDouble();
         System.out.println("5- Digite o numero da sessao");
         int n = sc.nextInt();
 
-        Sessao algumascoisa = new Sessao(new Filme(titulo,genero) , data, valor);
+        Sessao algumascoisa = new Sessao(new Filme(titulo,genero), horarioSessao, valor);
 
     }
 
