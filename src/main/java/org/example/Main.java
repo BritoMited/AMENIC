720be71df868450621e074a82f25fc32328dbee8
Brito <gustavo.ponciano2005@outlook.com>
2024-06-24 23:29:34 -0300

    teste de admview

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,17 @@
 package org.example;
 
+import org.example.Views.AdmView;
+import org.example.Views.ClienteView;
+
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
         Scanner scanner = new Scanner(System.in);
-        org.example.Views.ClienteView.iniciar(scanner);
+        AdmView.iniciarAdm(scanner);
 
     }
 }
