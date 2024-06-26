68b1541300aca4bb3c8e0479f461aa907b03c085
Brito <gustavo.ponciano2005@outlook.com>
2024-06-26 15:16:40 -0300

    adição das funcionalidades de pagamento, com taxas e verificação de meia entrada

--- src/main/java/org/example/Models/Pagamento/Pagamento.java
+++ src/main/java/org/example/Models/Pagamento/Pagamento.java
@@ -1,11 +1,14 @@
 package org.example.Models.Pagamento;
 
 import org.example.Models.Ingresso.Ingresso;
+import org.example.Models.Usuario.Cliente;
+
+import java.util.List;
 
 public interface Pagamento {
 
-    Ingresso gerarIngresso();
+    void gerarIngresso(List<Ingresso> listaIngresso, Cliente cliente);
 
-    Boolean verificarTipoIngresso();
+    Boolean verificarTipoIngresso(Boolean estudante, Integer idade);
 
 }
--- src/main/java/org/example/Models/Pagamento/PagamentoPayPal.java
+++ src/main/java/org/example/Models/Pagamento/PagamentoPayPal.java
@@ -1,15 +1,30 @@
 package org.example.Models.Pagamento;
 
 import org.example.Models.Ingresso.Ingresso;
+import org.example.Models.Usuario.Cliente;
+
+import java.util.List;
+
+import static org.example.Controllers.ClienteControllers.criarIngresso;
 
 public class PagamentoPayPal implements Pagamento{
+
     @Override
-    public Ingresso gerarIngresso() {
-        return null;
+    public void gerarIngresso(List<Ingresso> listaIngresso, Cliente cliente) {
+        for (Ingresso ingresso :listaIngresso){
+            if(verificarTipoIngresso(cliente.getEstudante(), cliente.getIdade())){
+                ingresso.setValor(ingresso.getValor()* 1.10/2);
+            }else{
+                ingresso.setValor(ingresso.getValor()* 1.10);
+            }
+            criarIngresso(ingresso);
+        }
     }
 
     @Override
-    public Boolean verificarTipoIngresso() {
-        return null;
+    public Boolean verificarTipoIngresso(Boolean estudante, Integer idade) {
+        if(estudante) return true;
+        else if (idade < 18) return true;
+        return false;
     }
 }
